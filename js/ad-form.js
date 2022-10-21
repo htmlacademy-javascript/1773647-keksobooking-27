e3b99183de713b3ec2cf6326a32313c7ce392260
SakharovaKsu <nagis38@gmail.com>
2022-10-21 11:57:50 +0300

    Добавила функцию на проверку валидности на количество комнат и гостей, добавила проверку на валидноть заголовка

--- js/ad-form.js
+++ js/ad-form.js
@@ -3,10 +3,54 @@ const adForm = document.querySelector('.ad-form');
 const pristine = new Pristine(adForm, {
   classTo: 'ad-form__label', // Элемент, на который будут добавляться классы
   errorClass: 'ad-form__element--invalid', // Класс, обозначающий невалидное поле
+  successClass: 'ad-form__element--valid', // Класс, обозначающий валидное поле
   errorTextParent: 'ad-form__element', // Элемент, куда будет выводиться текст с ошибкой
   errorTextTag: 'span', // Тег, который будет обрамлять текст ошибки
-  // errorTextClass: 'form__error' // Класс для элемента с текстом ошибки
+  errorTextClass: 'text-help' // Класс для элемента с текстом ошибки
 });
+
+//Проверяем на валидность заголовок
+const validateTitle = (value) => value.length >= 30 && value.length >= 100;
+
+pristine.addValidator(adForm.querySelector('#title'), validateTitle, 'От 30 до 100 символов');
+
+// Проверяем на валидность на количество комнат и количество мест
+const roomField = adForm.querySelector('#room_number');
+
+const maxAmount = {
+  1 : 1, // 1 комната — «для 1 гостя»;
+  2 : 2, // 2 комнаты — «для 2 гостей» или «для 1 гостя»;
+  3 : 3, // 3 комнаты — «для 3 гостей», «для 2 гостей» или «для 1 гостя»;
+  100 : 0, // 100 комнат — «не для гостей».
+};
+
+const validateAmount = (value) => {
+  const rooms = adForm.querySelector('[name="rooms"]');
+  return value.length <= maxAmount[rooms.value];
+};
+
+const getAmountErrorMessage = () => {
+  const rooms = adForm.querySelector('[name="rooms"]');
+  return `Не больше ${maxAmount[rooms.value]} гостей в комнату`;
+};
+
+const capacityField = adForm.querySelector('#capacity');
+const settlementOption = {
+  '1 комната': 'для 1 гостя',
+  '2 комнаты': ['для 2 гостей', 'для 1 гостя'],
+  '3 комнаты': ['для 3 гостей', 'для 2 гостей', 'для 1 гостя'],
+  '100 комнат': 'не для гостей',
+};
+
+const validateCapacity = () => settlementOption[capacityField.value].includes[roomField.value];
+
+const getValidationErrorMessage = () => `
+  ${capacityField.value}
+  ${roomField.value}
+  ${capacityField.value === 'Количество' ? 'возможно' : 'невозможно'}`;
+
+pristine.addValidator(roomField, validateAmount, getAmountErrorMessage, validateCapacity, getValidationErrorMessage);
+pristine.addValidator(capacityField,validateCapacity, getValidationErrorMessage);
 
 adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
 import './markup-elements.js';
 import './page-states.js';
+import './ad-form.js';
