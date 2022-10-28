7926a3fd67cf49ee332867aac1bfcae9c75b6b17
SakharovaKsu <nagis38@gmail.com>
2022-10-28 21:46:29 +0300

    Исправила ошибки

--- js/ad-form.js
+++ js/ad-form.js
@@ -7,35 +7,24 @@ const validationMessage = {
 /** @type {HTMLFormElement} */
 const adForm = document.querySelector('.ad-form');
 
-const pristine = new Pristine(adForm, {
-  classTo: 'ad-form__element', // Элемент, на который будут добавляться классы
-  errorClass: 'ad-form__element--invalid', // Класс, обозначающий невалидное поле
-  successClass: 'ad-form__element--valid',
-  errorTextParent: 'ad-form__element', // Элемент, куда будет выводиться текст с ошибкой
-  errorTextTag: 'span', // Тег, который будет обрамлять текст ошибки
-  errorTextClass: 'text-help' // Класс для элемента с текстом ошибки
-});
+const {
+  title: {dataset: titleDataset},
+  price: priceInput,
+  type: typeSelect,
+  timein: timeInSelect,
+  timeout: timeoutSelect,
+  rooms: roomsSelect,
+  capacity: capacitySelect,
+} = adForm.elements;
 
-// const titleDataset = adForm.elements.title.dataset;
-
-// titleDataset.pristineRequiredMessege = validationMessage.REQUIRED;
-// titleDataset.pristineMinlengthMessege = validationMessage.LENGTH;
-// titleDataset.pristineMaxlengthMessege = validationMessage.LENGTH;
-
-// const validateTitle = (value) => value.length >= 30 && value.length >= 100;
-
-// pristine.addValidator(validateTitle, validationMessage.LENGTH);
-
-
-/** @type {HTMLInputElement} */
-const priceInput = adForm.price;
 const priceDataset = priceInput.dataset;
 
-/** @type {HTMLSelectElement} */
-const typeSelect = adForm.elements.type;
+titleDataset.pristineRequiredMessage = validationMessage.REQUIRED;
+titleDataset.pristineMinlengthMessage = validationMessage.LENGTH;
+titleDataset.pristineMaxlengthMessage = validationMessage.LENGTH;
 
-priceDataset.pristineRequiredMessege = validationMessage.REQUIRED;
-// priceDataset.pristineMaxMessege = validationMessage.MAX_PRICE;
+priceDataset.pristineRequiredMessage = validationMessage.REQUIRED;
+priceDataset.pristineMaxMessage = validationMessage.MAX_PRICE;
 
 const typeToMinPrice = {
   bungalow: 0,
@@ -45,40 +34,38 @@ const typeToMinPrice = {
   palace: 10000,
 };
 
-pristine.addValidator(priceInput, (value) => {
-  const valuePrice = Number(value);
-  return valuePrice > 100000;
-}, validationMessage.MAX_PRICE);
-
-const updatePriceMinValue = () => {
-  const minPrice = typeToMinPrice[typeSelect.value];
-  priceInput.min = minPrice;
-  priceInput.placeholder = minPrice;
-  priceDataset.pristineMinMessege = `Минимальная цена ${minPrice}`;
-};
-
-updatePriceMinValue();
-
-const timeinSelect = adForm.elements.timein;
-const timeoutSelect = adForm.elements.timeout;
-
-timeinSelect.addEventListener('change', () => {
-  timeoutSelect.value = timeinSelect.value;
-});
-
-timeoutSelect.addEventListener('change', () => {
-  timeinSelect.value = timeoutSelect.value;
-});
-
-const roomsSelect = adForm.elements.rooms;
-const capacitySelect = adForm.elements.capacity;
-
 const roomsToCapacity = {
   '1': new Set(['1']),
   '2': new Set(['2', '1']),
   '3': new Set(['3', '2', '1']),
   '100': new Set(['0']),
 };
+
+const updatePriceMinValue = () => {
+  const minPrice = typeToMinPrice[typeSelect.value];
+  priceInput.min = minPrice;
+  priceInput.placeholder = minPrice;
+  priceDataset.pristineMinMessage = `Минимальная цена ${minPrice}`;
+};
+
+updatePriceMinValue();
+
+timeInSelect.addEventListener('change', () => {
+  timeoutSelect.value = timeInSelect.value;
+});
+
+timeoutSelect.addEventListener('change', () => {
+  timeInSelect.value = timeoutSelect.value;
+});
+
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element', // Элемент, на который будут добавляться классы
+  errorClass: 'ad-form__element--invalid', // Класс, обозначающий невалидное поле
+  successClass: 'ad-form__element--valid',
+  errorTextParent: 'ad-form__element', // Элемент, куда будет выводиться текст с ошибкой
+  errorTextTag: 'span', // Тег, который будет обрамлять текст ошибки
+  errorTextClass: 'text-help' // Класс для элемента с текстом ошибки
+});
 
 pristine.addValidator(capacitySelect, (value) => {
   const roomAmount = roomsSelect.value;
@@ -92,4 +79,5 @@ typeSelect.addEventListener('input', () => {
 
 adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
+  pristine.validate();
 });
