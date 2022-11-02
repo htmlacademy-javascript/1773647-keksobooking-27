ffb4a2c23abca3fc95954010744f617b20364f81
SakharovaKsu <nagis38@gmail.com>
2022-11-02 08:56:33 +0300

    Добавила вызовы функций в main

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ import './markup-elements.js';
 import './ad-form.js';
 import {initMap, createAdPinMarker, setAdPin, setOnMapLoad} from './map.js';
 
+setOnMapLoad(() => {
+  createAdPinMarker();
+  setAdPin(location);
+});
+
+initMap();
