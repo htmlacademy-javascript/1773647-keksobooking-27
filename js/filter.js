7982405bdd240412f965651c2653e23c377dcb97
SakharovaKsu <nagis38@gmail.com>
2022-11-16 16:32:02 +0300

    Рефакторинг filter

--- js/filter.js
+++ js/filter.js
@@ -6,109 +6,113 @@ const priceHousing = {
   MIN: 10000,
   MAX: 50000,
 };
-
 const RERENDER_DELAY = 1000;
+const FILTER_ALL = 'any';
 
 /** @type {HTMLFormElement} */
 const filterForm = document.querySelector('.map__filters');
 
-const {
-  ['housing-type']: selectType,
-  ['housing-price']: selectPrice,
-  ['housing-rooms']: selectRooms,
-  ['housing-guests']: selectGuests,
-} = filterForm;
+const createFilters = () => ({
+  type: filterForm['housing-type'].value,
+  price: filterForm['housing-price'].value,
+  rooms: filterForm['housing-rooms'].value,
+  guests: filterForm['housing-guests'].value,
+});
 
-const {
-  ['filter-wifi']: checkboxWifi,
-  ['filter-parking']: checkboxDishwasher,
-  ['filter-dishwasher']: checkboxParking,
-  ['filter-elevator']: checkboxElevator,
-  ['filter-washer']: checkboxWasher,
-  ['filter-conditioner']: checkboxConditioner,
-} = filterForm;
+const createFeatures = () => {
+  const checkboxes = [
+    filterForm['filter-wifi'],
+    filterForm['filter-parking'],
+    filterForm['filter-dishwasher'],
+    filterForm['filter-elevator'],
+    filterForm['filter-washer'],
+    filterForm['filter-conditioner'],
+  ];
 
-const checkboxesFilter = [
-  checkboxWifi,
-  checkboxDishwasher,
-  checkboxParking,
-  checkboxWasher,
-  checkboxElevator,
-  checkboxConditioner
-];
-
-/**
- * @param {{offer: {features: String[]}}} offer
- * @param {String[]} name
- * @returns
- */
-const filterBycheckbox = (offer, name) => {
-  switch (name) {
-    case 'wifi':
-      return offer.features && offer.features.includes('wifi');
-    case 'dishwasher':
-      return offer.features && offer.features.includes('dishwasher');
-    case 'parking':
-      return offer.features && offer.features.includes('parking');
-    case 'washer':
-      return offer.features && offer.features.includes('washer');
-    case 'elevator':
-      return offer.features && offer.features.includes('elevator');
-    case 'conditioner':
-      return offer.features && offer.features.includes('conditioner');
-  }
+  return checkboxes.filter((checkbox) => checkbox.checked).map((checkbox) => checkbox.value);
 };
 
+const isFilteringByFeatures = (offer, features) =>
+  offer.features ?
+    features.every((feature) =>
+      offer.features.includes(feature)
+    ) :
+    false;
+
 /**
-   * @param {{offer: {price: Number}}} offer
-   * @param {Number} price
-   * @return {Boolean}
+ * @param {{offer: {price: Number}}} offer
+ * @param {Number} price
+ * @return {Boolean}
  */
-const filterByPrice = (offer, price) => {
+const isFilteringByPrice = (offer, price) => {
   switch (price) {
-    case 'any':
-      return true;
     case 'low':
       return offer.price < priceHousing.MIN;
     case 'middle':
       return offer.price < priceHousing.MAX && offer.price > priceHousing.MIN;
     case 'high':
       return offer.price >= priceHousing.MAX;
+    default: // any
+      return true;
   }
 };
 
-filterForm.addEventListener('input', ({target}) => {
+const isFilteringByValue = (value, filterValue) => filterValue === FILTER_ALL ? true : String(value) === String(filterValue);
+
+const isFilteringLocation = (location, filters) => {
+  const { offer } = location;
+
+  const filteredByType = isFilteringByValue(offer.type, filters.type);
+  if (!filteredByType) {
+    return false;
+  }
+  const filteredByRooms = isFilteringByValue(offer.rooms, filters.rooms);
+  if (!filteredByRooms) {
+    return false;
+  }
+
+  const filteredByGuests = isFilteringByValue(offer.guests, filters.guests);
+  if (!filteredByGuests) {
+    return false;
+  }
+
+  const filteredByPrice = isFilteringByPrice(offer, filters.price);
+  if (!filteredByPrice) {
+    return false;
+  }
+
+  return true;
+};
+
+/** Фильтруем локации по фильтрам */
+const filterLocationsByFilters = (locations) => {
+  const filters = createFilters();
+  const filteredLocations = locations.filter((location) => isFilteringLocation(location, filters));
+
+  return filteredLocations;
+};
+
+/** Фильтруем локации по удобствам */
+const filterLocationsByFeatures = (locations) => {
+  const features = createFeatures();
+
+  return locations.filter(({ offer }) => isFilteringByFeatures(offer, features));
+};
+
+
+filterForm.addEventListener('input', () => {
   const locations = getAllLocations();
+  const filteredLocations = filterLocationsByFilters(locations);
 
-  if(target === selectType) {
-    const filteredLocations = locations.filter(({offer}) => offer.type === selectType.value);
-    setAdPins(filteredLocations);
-  }
+  setAdPins(filteredLocations);
 
-  if(target === selectPrice) {
-    const filteredPrice = locations.filter(({ offer }) => filterByPrice(offer, selectPrice.value));
-    setAdPins(filteredPrice);
-  }
-
-  if(target === selectRooms) {
-    const filteredRooms = locations.filter(({offer}) => offer.rooms === +selectRooms.value);
-    setAdPins(filteredRooms);
-  }
-
-  if(target === selectGuests) {
-    const filteredGuests = locations.filter(({offer}) => offer.guests === +selectGuests.value);
-    setAdPins(filteredGuests);
-  }
-
-  const filteredAccommodations = () => {
-    if (checkboxesFilter.includes(target)) {
-      const filtred = locations.filter(({offer}) => filterBycheckbox(offer, target.value));
-      setAdPins(filtred);
-    }
+  const filterByFeatures = () => {
+    const filteredLocationsByFeatures = filterLocationsByFeatures(filteredLocations);
+    setAdPins(filteredLocationsByFeatures);
   };
 
-  const setOnFilterChange = debounce(filteredAccommodations, RERENDER_DELAY);
-  setOnFilterChange();
+  const setAdPinsDebounce = debounce(filterByFeatures, RERENDER_DELAY);
+  setAdPinsDebounce();
 });
 
 export { filterForm };
